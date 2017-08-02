0f729314b7ddfa1fded5b8e85f8b4fdf45645966
范洲平 <fanzp@knownsec.com>
2017-08-02 21:06:29 +0800

    rect, line, arc

--- canvas.js
+++ canvas.js
@@ -4,4 +4,25 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
+
+// Rectangles
+c.fillStyle = 'rgba(255, 0, 0, 0.5)';
 c.fillRect(100, 100, 100, 100);
+c.fillStyle = 'rgba(0, 255, 0, 0.5)';
+c.fillRect(400, 100, 100, 100);
+c.fillStyle = 'rgba(0, 0, 255, 0.5)';
+c.fillRect(300, 300, 100, 100);
+
+// Line
+c.beginPath();
+c.lineTo(50, 300);
+c.lineTo(300, 100);
+c.lineTo(400, 300);
+c.strokeStyle = "#fa34a3";
+c.stroke();
+
+// Arc & Circle
+c.beginPath();
+c.arc(300, 300, 30, 0, Math.PI * 2, false);
+c.strokeStyle = 'blue'
+c.stroke();
