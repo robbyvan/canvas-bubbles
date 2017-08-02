3bf8c7bde9ea8608d25aacb042cc52897e347bc1
范洲平 <fanzp@knownsec.com>
2017-08-02 21:39:14 +0800

    Object: circle

--- canvas.js
+++ canvas.js
@@ -5,34 +5,49 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d');
 
+function Circle(x, y, dx, dy, radius, ) {
+  this.x = x;
+  this.y = y;
+  this.dx = dx;
+  this.dy = dy;
+  this.radius = radius;
+  
+  this.draw = function() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.strokeStyle = "#fa34a3";
+    c.stroke();
+  };
+
+  this.update = function() {
+    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;  
+    }
+    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;  
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
+  };
+
+
+
+}
+
+var circle = new Circle(200, 300, 5, 5, 35);
 
 var radius = 30;
-var x = Math.random() * innerWidth + radius;
+var x = Math.random() * innerWidth;
 var dx = (Math.random() - 0.5) * 8;
-var y = Math.random() * innerHeight + radius;
+var y = Math.random() * innerHeight;
 var dy = (Math.random() - 0.5) * 8;
-
-console.log(x, y, dx, dy, innerWidth, innerHeight, radius);
 
 function animateCircles() {
   requestAnimationFrame(animateCircles);
   c.clearRect(0, 0, innerWidth, innerHeight);
 
-  c.beginPath();
-  c.arc(x, y, radius, 0 , Math.PI * 2);
-  c.strokeStyle = "#fa34a3";
-  c.stroke();
-
-  if (x + radius > window.innerWidth || x - radius < 0) {
-    dx = -dx;  
-  }
-
-  if (y + radius > window.innerHeight || y - radius < 0) {
-    dy = -dy;  
-  }
-
-  x += dx;
-  y += dy;
+  circle.update();
   
 }
 
